327534adc12551cf0e98f4806b10570162172991
Thinhskyduck <thinhskyduck@gmail.com>
2024-10-22 17:25:23 +0700

    lan 2

---
+++ TEDU/Areas/Admin/Controllers/RouteController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TEDU.Areas.Admin.Controllers
+{
+    public class RouteController : Controller
+    {
+        // GET: Admin/Route
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        public ActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
